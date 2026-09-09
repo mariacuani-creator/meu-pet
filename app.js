d598d5db75fab9a155b085805acd419b5a39aee8
mariacuani-creator <maria.cuani@escola.pr.gov.br>
2026-09-09 07:34:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("oi");
+async function imagemApi(){
+    
+    let respostaApi = await fetch('https://dog.ceo/api/breeds/image/random');
+    let dadosApi = await respostaApi.json();
+    document.querySelector(".img-dog").src = dadosApi.message;
+    console.log(respostaApi);
+}
