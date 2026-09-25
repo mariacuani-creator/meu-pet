9ceafeb40b4a351e091b2aa97d11992ea753e106
mariacuani-creator <maria.cuani@escola.pr.gov.br>
2026-09-25 11:16:47 -0300

    v.8

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ async function imagemApi(){
     let respostaApi = await fetch('https://dog.ceo/api/breeds/image/random');
     let dadosApi = await respostaApi.json();
     document.querySelector(".img-dog").src = dadosApi.message;
-    console.log(respostaApi);
+    console.log(dadosApi);
 }
+
+imagemApi()
